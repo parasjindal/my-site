a4e8f9862c63a1db9d9b5ff3b9f0fd214d706576
Paras Jindal <pajindal@microsoft.com>
2018-09-15 10:39:32 +0530

    Header added

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './../css/header.css'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div class = "header">
+                Welcome to Paras's website.
+            </div>
+        );
+    }
+}
+
+
